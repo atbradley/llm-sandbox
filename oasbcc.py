2a8f42d23545d5a051ed265e5f4a7688a24ed871
Adam Bradley <me@adambradley.net>
2024-05-18 17:00:17 -0400

    Updating to new version of the `openai` module.

---
+++ oasbcc.py
@@ -0,0 +1,48 @@
+import os
+import json
+
+import yaml
+import time
+
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
+MODEL = os.getenv("MODEL", "gpt-4") 
+openai.api_key = os.getenv("API_KEY")
+
+sys_msg = input("Enter the system message: ")
+messages = [
+            {"role": "system", "content": sys_msg},
+]
+
+filename = f"convo{time.time_ns()}.yaml"
+
+while True:
+    print()
+    prompt = input("Enter your text or 'q' to quit: ")    
+    
+    if prompt == "q":
+        break
+
+    messages.append({"role": "user", "content": prompt})
+    print("Sending...")
+
+    response=openai.chat.completions.create(
+        model=MODEL,
+        messages=messages,
+        n=1  # We can ask for more possible responses.
+    )
+
+    print("Response:", response.choices[0].message.content)
+    messages.append({
+        "role": response.choices[0].message.role,
+        "content": response.choices[0].message.content,
+    })
+    print("Tokens: ", response.usage.total_tokens)
+
+    yaml.dump(messages, open(os.path.join(OUTPUT_FOLDER, filename), "w"), indent=4, Dumper=yaml.SafeDumper)
+
+#print(messages)
